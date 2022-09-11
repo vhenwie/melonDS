0329aa2f103ffb26755e6f31de4cfe0687e6d815
Henry Hu <henry.hu.sh@gmail.com>
2022-09-11 18:41:14 -0400

    fix build on FreeBSD

--- src/libretro/platform.cpp
+++ src/libretro/platform.cpp
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #define socket_t    int
 #define sockaddr_t  struct sockaddr
 #define closesocket close
